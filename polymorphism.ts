45f7e85bf85f34d67e13db119fca5cb3bd8e072b
constantly-dev <rntqhdl10@naver.com>
2024-07-26 16:37:08 +0900

    polymorphism 구현

---
+++ polymorphism.ts
@@ -0,0 +1,110 @@
+{
+  // 추상화는  클래스를 간편하게 쓰기위해서 어떻게 꾸며야하는지,
+  // 공통점을 찾고 불필요한  세부사항은 제거해서  클래스를 간단하게 만드는 단계이고
+  // 캡슐화는 클래스를 만들때 외부에서 맘대로 사용하지못하도록 데이터를 보호하는 역할
+
+  type CoffeeCup = {
+    shots: number;
+    hasMilk?: boolean;
+    hasSugar?: boolean;
+  };
+
+  interface CoffeeMaker {
+    makeCoffee(shots: number): CoffeeCup;
+  }
+
+  class CoffeeMachine implements CoffeeMaker {
+    private static BEANS_GRAM_PER_SHOT: number = 7; // class level
+    private coffeeBeans: number = 0; // instance (object) level
+
+    public constructor(coffeeBeans: number) {
+      this.coffeeBeans = coffeeBeans;
+    }
+
+    static makeMachine(coffeeBeans: number): CoffeeMachine {
+      return new CoffeeMachine(coffeeBeans);
+    }
+
+    fillCoffeeBeans(beans: number) {
+      if (beans < 0) {
+        throw new Error('value for beans should be greater than 0');
+      }
+      this.coffeeBeans += beans;
+    }
+
+    clean() {
+      console.log('cleaning the machine...');
+    }
+
+    private grindBeans(shots: number) {
+      console.log(`grinding beans for ${shots}`);
+      if (this.coffeeBeans < shots * CoffeeMachine.BEANS_GRAM_PER_SHOT) {
+        throw new Error('Not enough coffee beans!');
+      }
+      this.coffeeBeans -= shots * CoffeeMachine.BEANS_GRAM_PER_SHOT;
+    }
+
+    private preheat(): void {
+      console.log('heating up...');
+    }
+
+    private extract(shots: number): CoffeeCup {
+      console.log(`pulling ${shots} shots`);
+      return {
+        shots: shots,
+        hasMilk: false,
+      };
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      this.grindBeans(shots);
+      this.preheat();
+      return this.extract(shots);
+    }
+  }
+
+  class CafeLatteMachine extends CoffeeMachine {
+    constructor(beans: number, public readonly serialNumber: string) {
+      super(beans);
+    }
+    private steamMilk(): void {
+      console.log('steaming some milk...');
+    }
+
+    makeCoffee(shots: number): CoffeeCup {
+      const coffee = super.makeCoffee(shots);
+
+      return {
+        ...coffee,
+        hasMilk: true,
+      };
+    }
+  }
+
+  class SweetCoffeeMaker extends CoffeeMachine {
+    makeCoffee(shots: number): CoffeeCup {
+      const coffee = super.makeCoffee(shots);
+      return {
+        ...coffee,
+        hasSugar: true,
+      };
+    }
+  }
+
+  const machines: CoffeeMaker[] = [
+    new CoffeeMachine(16),
+    new CafeLatteMachine(16, '1'),
+    new SweetCoffeeMaker(16),
+    new CoffeeMachine(16),
+    new CafeLatteMachine(16, '1'),
+    new SweetCoffeeMaker(16),
+  ];
+
+  machines.forEach((machine) => {
+    console.log('------------------------');
+    machine.makeCoffee(1);
+  });
+
+  // 다형성의 장점
+  // 어떤 클래스인지 구분하지 않고 공통된 api 호출(함수) 가능
+}
